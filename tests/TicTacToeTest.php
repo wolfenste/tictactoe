156303c7fb3883be9e961afcfe219fec7c4710b6
wolfenste <wolfenstecg@gmail.com>
2018-07-18 17:59:07 +0000

    Another use cases.

--- tests/TicTacToeTest.php
+++ tests/TicTacToeTest.php
@@ -18,5 +18,63 @@ class TicTacToeTest extends TestCase {
 	$this->assertTrue (new Mark ('X')->equals ($game->getCurrentMark ()));
 	$this->assertTrue ($game->isMapEmpty ());
     }
+
+    /**
+     * @test
+     */
+    public function the_map_is_completed_no_winner () {
+        $map = new Map ();
+	$playerX = new Player ();
+	$player0 = new Player ();
+	$game = new Game ($playerX, $player0, $map);
+
+	$this->assertTrue ($game->isMapCompleted ());
+	$this->assertFalse ($game->playerXWon ());
+	$this->assertFalse ($game->player0Won ());
+	$this->assertTrue ($game->isGameFinished ());
+    }
+
+    /**
+     * @test
+     */
+    public function player_x_won_the_game () {
+	$map = new Map ();
+	$playerX = new Player ();
+	$player0 = new Player ();
+	$game = new Game ($playerX, $player0, $map);
+
+	$this->assertTrue ($game->playerXWon ());
+	$this->assertFalse ($game->player0Won ());
+	$this->assertTrue ($game->isGameFinished ());
+    }
+
+    /**
+     * @test
+     */
+    public function player_0_won_the_game () {
+	$map = new Map ();
+	$playerX = new Player ();
+	$player0 = new Player ();
+	$game = new Game ($playerX, $player0, $map);
+
+	$this->assertTrue ($game->player0Won ());
+	$this->assertFalse ($game->playerXWon ());
+	$this->assertTrue ($game->isGameFinished ());
+    }
+
+    /**
+     * @test
+     */
+    public function no_winners_still_playing_prepare_next_move () {
+	$map = new Map ();
+	$playerX = new Player ();
+	$player0 = new Player ();
+	$game = new Game ($playerX, $player0, $map);
+
+	$this->assertFalse ($game->playerXWon ());
+	$this->assertFalse ($game->player0Won ());
+	$this->assertFalse ($game->isGameFinished ());
+	$this->assertTrue ($game->mapUpdated ());
+    }
 }
 
