62bfcd8430f0713e51fdc3c16dd803d094664704
wolfenste <wolfenstecg@gmail.com>
2018-07-28 19:57:49 +0000

    Yet another shape for the application.

--- tests/TicTacToeTest.php
+++ tests/TicTacToeTest.php
@@ -2,7 +2,6 @@
 
 use PHPUnit\Framework\TestCase;
 use TicTacToe\Mark;
-use TicTacToe\Square;
 use TicTacToe\Map;
 use TicTacToe\Player;
 use TicTacToe\Game;
@@ -12,63 +11,67 @@ class TicTacToeTest extends TestCase {
      * @test
      */
     public function start_the_game_with_an_empty_map_the_current_player_being_x () {
-	$mark = new Mark ('X');
-	$square = null; // no input at this moment
-        $map = new Map ($marks); // initial values
-	$playerX = new Player ('X', $linesX);
-	$player0 = new Player ('0', $lines0);
-	$game = new Game ($playerX, $player0, $map, $square, $mark);
-
-	$this->assertTrue (new Mark ('X')->equals ($game->getCurrentMark ()));
+	$playerX = new Player (new Mark ('X'));
+	$player0 = new Player (new Mark ('0'));
+	$marks = array (
+		'(1, 1)' => '', '(1, 2)' => '', '(1, 3)' => '',
+		'(2, 1)' => '', '(2, 2)' => '', '(2, 3)' => '',
+		'(3, 1)' => '', '(3, 2)' => '', '(3, 3)' => ''
+	);
+	$map = new Map ($marks);
+	$game = new Game ($playerX, $playerY, $map);
+	
+	$this->assertEquals ($game->getGameStatus (), 'start');
 	$this->assertTrue ($game->isMapEmpty ());
-
-	$this->assertEquals ($game->getGameStatus (), 'start');
 	$this->assertEquals ($game->getWhoWon (), '');
-	$game->playGame ();
-	$this->assertEquals ($game->getWhoWon (), '');
-	$this->assertFalse ($game->isMapCompleted ());
-	$this->assertEquals ($game->getGameStatus (), 'start');
     }
 
     /**
      * @test
      */
     public function the_map_is_completed_no_winner () {
-	$mark = new Mark ($currentMark);
-	$square = new Square ($x, $y, $mark);
-        $map = new Map ($marks);
-	$playerX = new Player ('X', $linesX);
-	$player0 = new Player ('0', $lines0);
-	$game = new Game ($playerX, $player0, $map, $square, $mark);
-
+	$playerX = new Player (new Mark ('X'));
+	$player0 = new Player (new Mark ('0'));
+	$marks = array (
+		'(1, 1)' => 'X', '(1, 2)' => '0', '(1, 3)' => 'X',
+		'(2, 1)' => '', '(2, 2)' => '', '(2, 3)' => '',
+		'(3, 1)' => '', '(3, 2)' => 'X', '(3, 3)' => '0'
+	);
+	$map = new Map ($marks);
+	$game = new Game ($playerX, $playerY, $map);
+        
 	$this->assertFalse ($game->isMapEmpty ());
-
+	$player0->moveToSquare (2, 1);
+	$playerX->moveToSquare (2, 2);
+	$player0->moveToPlayer (3, 1);
 	$this->assertEquals ($game->getGameStatus (), 'in_progress');
-	$this->assertEquals ($game->getWhoWon (), '');
-	$game->playGame ();
-	$this->assertEquals ($game->getWhoWon (), '');
+	$playerX->moveToSquare (2, 3);
 	$this->assertTrue ($game->isMapCompleted ());
 	$this->assertEquals ($game->getGameStatus (), 'end');
+	$this->assertEquals ($game->getWhoWon (), '');
     }
 
     /**
      * @test
      */
     public function player_x_won_the_game () {
-	$mark = new Mark ('X');
-	$square = new Square ($x, $y, $mark);
-	$map = new Map ($marks);
-	$playerX = new Player ('X', $linesX);
-	$player0 = new Player ('0', $lines0);
-	$game = new Game ($playerX, $player0, $map, $square, $mark);
-	
-	$this->assertTrue (new Mark ('X')->equals ($game->getCurrentMark ()));
-	$this->assertFalse ($game->isMapEmpty ());
+        $playerX = new Player (new Mark ('X'));
+	$player0 = new Player (new Mark ('0'));
+	$marks = array (
+		'(1, 1)' => '', '(1, 2)' => '', '(1, 3)' => '',
+		'(2, 1)' => '', '(2, 2)' => '', '(2, 3)' => '',
+		'(3, 1)' => '', '(3, 2)' => '', '(3, 3)' => ''
+	);
+        $map = new Map ($marks);
+	$game = new Game ($playerX, $player0, $map);
 
-	$this->assertEquals ($game->getGameStatus (), 'in_progress');
-	$this->assertEquals ($game->getWhoWon (), '');
-	$game->playGame ();
-	$game->assertEquals ($game->getWhoWon (), 'player_x');
+	$playerX->moveToSquare (1, 1);
+	$this->assertFalse ($game->isMapEmpty ());
+	$player0->moveToSquare (2, 1);
+	$playerX->moveToSquare (1, 2);
+	$player0->moveToSquare (2, 2);
+	$playerX->moveToSquare (1, 3);
+        $this->assertEquals ($game->getWhoWon (), 'playerX');
 	$this->assertEquals ($game->getGameStatus (), 'end');
     }
 
@@ -76,20 +79,24 @@ class TicTacToeTest extends TestCase {
      * @test
      */
     public function player_0_won_the_game () {
-	$mark = new Mark ('0');
-	$square = new Square ($x, $y, $mark);
+	$playerX = new Player (new Mark ('X'));
+	$player0 = new Player (new Mark ('0'));
+	$marks = array (
+		'(1, 1)' => '', '(1, 2)' => '', '(1, 3)' => '',
+		'(2, 1)' => '', '(2, 2)' => '', '(2, 3)' => '',
+		'(3, 1)' => '', '(3, 2)' => '', '(3, 3)' => ''
+	);
 	$map = new Map ($marks);
-	$playerX = new Player ('X', $linesX);
-	$player0 = new Player ('0', $lines0);
-	$game = new Game ($playerX, $player0, $map, $square, $mark);
-
-	$this->assertTrue (new Mark ('0')->equals ($game->getCurrentMark ()));
+	$game = new Game ($playerX, $player0, $map);
+        
+	$playerX->moveToSquare (1, 1);
 	$this->assertFalse ($game->isMapEmpty ());
-
-	$this->assertEquals ($game->getGameStatus (), 'in_progress');
-	$this->assertEquals ($game->getWhoWon (), '');
-	$game->playGame ();
-	$this->assertEquals ($game->getWhoWon (), 'player_0');
+	$player0->moveToSquare (3, 3);
+	$playerX->moveToSquare (2, 2);
+	$player0->moveToSquare (1, 3);
+	$playerX->moveToSquare (3, 1);
+	$player0->moveToSquare (2, 3);
+	$this->assertEquals ($game->getWhoWon (), 'player0');
 	$this->assertEquals ($game->getGameStatus (), 'end');
     }
 
@@ -97,21 +104,23 @@ class TicTacToeTest extends TestCase {
      * @test
      */
     public function no_winners_still_playing_prepare_next_move () {
-	$mark = new Mark ($currentMark);
-	$square = new Square ($x, $y, $mark);
+	$playerX = new Player (new Mark ('X'));
+	$player0 = new Player (new Mark ('0'));
+	$marks = array (
+		'(1, 1)' => '', '(1, 2)' => '', '(1, 3)' => '',
+		'(2, 1)' => '', '(2, 2)' => '', '(2, 3)' => '',
+		'(3, 1)' => '', '(3, 2)' => '', '(3, 3)' => ''
+	);
 	$map = new Map ($marks);
-	$playerX = new Player ('X', $linesX);
-	$player0 = new Player ('0', $lines0);
-	$game = new Game ($playerX, $player0, $map, $square, $mark);
-
+	$game = new Game ($playerX, $player0, $map);
+	
+	$player->moveToSquare (1, 2);
 	$this->assertFalse ($game->isMapEmpty ());
-
-	$this->assertEquals ($game->getGameStatus (), 'in_progress');
-	$this->assertEquals ($game->getWhoWon (), '');
-	$game->playGame ();
-	$game->assertEquals ($game->getWhoWon (), '');
+	$player0->moveToSquare (2, 1);
 	$this->assertFalse ($game->isMapCompleted ());
 	$this->assertEquals ($game->getGameStatus (), 'in_progress');
+	$this->assertEquals ($game->getWhoWon (), '');
     }
+
 }
 
