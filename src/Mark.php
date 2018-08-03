9dbbe86625eccea90b934a89a0146218e37d5427
wolfenste <wolfenstecg@gmail.com>
2018-08-03 02:06:36 +0000

    Introducing method getValue in class Mark.

--- src/Mark.php
+++ src/Mark.php
@@ -6,5 +6,12 @@ class Mark {
     const SYMBOL_X = 'X';
     const SYMBOL_0 = '0';
     const SYMBOL_NONE = '';
+
+    /**
+     * @return char
+     */
+    public function getValue () {
+        return $this->symbol_value;
+    }
 }
 
