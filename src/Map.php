b416121bede214ae884c4cba44f3ddaec2a9699d
wolfenste <wolfenstecg@gmail.com>
2018-08-03 01:26:01 +0000

    Validation for Map constructor's parameter.

--- src/Map.php
+++ src/Map.php
@@ -12,8 +12,26 @@ class Map {
      * @param $marks, array containing 9 Mark objects
      */
     public function __construct ($marks) {
-	$this->marks = $marks;
-    }
+	try {
+	    if (is_array ($marks) && count ($marks) == 9) {
+		$validator = true;
+		foreach ($marks as $key => $val) {
+		    if (get_class ($val) != 'Mark') {
+			$validator = false;
+		    }
+		}
+		if ($validator) {
+		    $this->marks = $marks;
+		} else {
+		    throw new Exception ('Invalid element in Map constructor\'s parameter');
+		}
+	    } else {
+		throw new Exception ('Invalid Map constructor\'s parameter');
+	    }
+	} catch (Exception $e) {
+	    echo $e->getMessage ();
+	}
+    }	
 
     /**
      * @return array
