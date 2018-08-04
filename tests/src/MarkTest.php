fafc9b8b8e5de2c6417d1ea043caa2f084a6be1a
wolfenste <wolfenstecg@gmail.com>
2018-08-04 03:23:18 +0000

    Fix test mark_is_constructed_with_value_none version2.

--- tests/src/MarkTest.php
+++ tests/src/MarkTest.php
@@ -9,6 +9,6 @@ class MarkTest extends PHPUnit\Framework\TestCase {
      */
     public function mark_is_constructed_with_value_none () {
 	$mark = new Mark (Mark::SYMBOL_NONE);
-	$this->assertTrue ($mark->getValue () === Mark::SYMBOL_NONE);
+	$this->assertTrue ((new Mark (Mark::SYMBOL_NONE))->equal ($mark));
     }
 }
