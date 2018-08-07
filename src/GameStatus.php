04e7b4332e6aba33148f7aa420b6fcd35353e1d8
wolfenste <wolfenstecg@gmail.com>
2018-08-07 14:06:26 +0000

    Introducing class constants for GameStatus.

--- src/GameStatus.php
+++ src/GameStatus.php
@@ -3,5 +3,20 @@
 namespace TicTacToe;
 
 class GameStatus {
+
+    /**
+     * @const the game is at the beginning
+     */
+	const STATUS_START = 'status_start';
+
+    /**
+     * @const the game is in progress
+     */
+	const STATUS_IN_PROGRESS = 'status_in_progress';
+
+    /**
+     * @const the game is finished
+     */
+    const STATUS_END = 'status_end';
 }
 
