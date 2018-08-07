78548ed5599edc11793443a42def4dd69e91b878
wolfenste <wolfenstecg@gmail.com>
2018-08-07 14:52:55 +0000

    Introducing and ... passing the test: game_status_is_constructed_with_value_status_start.

--- tests/src/GameStatusTest.php
+++ tests/src/GameStatusTest.php
@@ -4,4 +4,13 @@ use PHPUnit\Framework\TestCase;
 use TicTacToe\GameStatus;
 
 class GameStatusTest extends TestCase {
+    /**
+     * @test
+     */
+    public function game_status_is_constructed_with_value_status_start () {
+	$gameStatus = new GameStatus (GameStatus::STATUS_START);
+	$this->assertTrue (
+	    (new GameStatus (GameStatus::STATUS_START))->equals ($gameStatus)
+        );
+    }
 }
