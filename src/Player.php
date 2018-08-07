0afa7e08056e8ec998531cbd013e57003d87d2f1
wolfenste <wolfenstecg@gmail.com>
2018-08-07 13:40:27 +0000

    test_player_construction_with_symbol_x passed.

--- src/Player.php
+++ src/Player.php
@@ -3,6 +3,30 @@
 namespace TicTacToe;
 
 class Player {
-    public function __construct () {
+    /**
+     * @var, Mark object
+     */
+    private $playerName;
+
+    /**
+     * Class constructor
+     *
+     * @param $mark, Mark object
+     */
+    public function __construct (Mark $mark) {
+	if (get_class ($mark) !== Mark::class) {
+	    throw new \InvalidArgumentException (
+	        'The argument must be a Mark object.'
+	    );
+	}
+
+	$this->playerName = $mark;
+    }
+
+    /**
+     * @return Mark object
+     */
+    public function getPlayerName () {
+	return $this->playerName;
     }
 }
--- tests/src/PlayerTest.php
+++ tests/src/PlayerTest.php
@@ -5,7 +5,7 @@ use TicTacToe\Player;
 use TicTacToe\Mark;
 
 class PlayerTest extends PHPUnit\Framework\TestCase {
-    public function test_player_construction () {
+    public function test_player_construction_with_symbol_x () {
 	    $playerX = new Player (new Mark (Mark::SYMBOL_X));
 	$this->assertTrue ((new Mark (Mark::SYMBOL_X))->equal ($playerX->getPlayerName ()));
     }
