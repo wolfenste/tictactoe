985cf220370d590d50662582d1f42680079bbc66
wolfenste <wolfenstecg@gmail.com>
2018-08-08 13:57:49 +0000

    Introducing and passing the test game_is_constructed_with_an_empty_map.

--- src/Game.php
+++ src/Game.php
@@ -3,5 +3,49 @@
 namespace TicTacToe;
 
 class Game {
+    /**
+     * @var Player object
+     */
+    private $playerX;
+
+    /**
+     * @var Player object
+     */
+    private $player0;
+
+    /**
+     * @var Map object
+     */
+    private $map;
+
+    /***
+     * Class constructor
+     * @param $playerX Player object
+     * @param $player0 Player object
+     * @param $map Map object
+     */
+    public function __construct ($playerX, $player0, $map) {
+	if (!is_object ($playerX) || get_class ($playerX) != Player::class) {
+	    throw new \InvalidArgumentException (
+		'The first paramater must be a Player object.'
+	    );
+	}
+
+	if (!is_object ($player0) || get_class ($player0) != Player::class) {
+	    throw new \InvalidArgumentException (
+		'The second parameter must be a Player object.'
+	    );
+	}
+
+	if (!is_object ($map) || get_class ($map) != Map::class) {
+	    throw new \InvalidArgumentException (
+		'The third parameter must be a Map object.'
+	    );
+	}
+
+	$this->playerX = $playerX;
+	$this->player0 = $player0;
+	$this->map = $map;
+    }
 }
 
--- tests/src/GameTest.php
+++ tests/src/GameTest.php
@@ -4,7 +4,18 @@ use TicTacToeTests\BaseClassTest;
 use TicTacToe\Game;
 use TicTacToe\Player;
 use TicTacToe\Map;
+use TicTacToe\Mark;
 
-class GameTest extends BaseTestClass {
+class GameTest extends BaseClassTest {
+    /**
+     * @test
+     */
+    public function game_is_constructed_with_an_empty_map () {
+	$playerX = new Player (new Mark (Mark::SYMBOL_X));
+	$player0 = new Player (new Mark (Mark::SYMBOL_0));
+	$map = new Map ($this->createEmptyTableSpec ());
+        $game = new Game ($playerX, $player0, $map);
 
+	$this->assertTrue (is_object ($game) && (get_class ($game) === Game::class));
+    }
 }
