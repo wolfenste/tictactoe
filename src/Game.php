4e8e00a85a5b536685de411b2be8def85c3ca613
wolfenste <wolfenstecg@gmail.com>
2018-08-09 14:03:14 +0000

    Using type hinting. No need for exceptions here.

--- src/Game.php
+++ src/Game.php
@@ -24,25 +24,7 @@ class Game {
      * @param $player0 Player object
      * @param $map Map object
      */
-    public function __construct ($playerX, $player0, $map) {
-	if (!is_object ($playerX) || get_class ($playerX) != Player::class) {
-	    throw new \InvalidArgumentException (
-		'The first paramater must be a Player object.'
-	    );
-	}
-
-	if (!is_object ($player0) || get_class ($player0) != Player::class) {
-	    throw new \InvalidArgumentException (
-		'The second parameter must be a Player object.'
-	    );
-	}
-
-	if (!is_object ($map) || get_class ($map) != Map::class) {
-	    throw new \InvalidArgumentException (
-		'The third parameter must be a Map object.'
-	    );
-	}
-
+    public function __construct (Player $playerX, Player $player0, Map $map) {
 	$this->playerX = $playerX;
 	$this->player0 = $player0;
 	$this->map = $map;
