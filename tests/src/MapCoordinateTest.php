a28287af5b99eae65512570345385cc3da754933
wolfenste <wolfenstecg@gmail.com>
2018-08-10 13:19:57 +0000

    Adding test: mapcoordinate is constructed with valid arguments.

--- tests/src/MapCoordinateTest.php
+++ tests/src/MapCoordinateTest.php
@@ -4,5 +4,15 @@ use TicTacToeTests\BaseClassTest;
 use TicTacToe\MapCoordinate;
 
 class MapCoordinateTest extends BaseClassTest {
+    /**
+     * @test
+     */
+    public function map_coordinate_is_constructed_with_valid_arguments () {
+        $mapCoordinate = new MapCoordinate (2, 2);
+        $this->assertTrue (
+            is_object ($mapCoordinate) &&
+            get_class ($mapCoordinate) === MapCoordinate::class
+        );
+    }
 }
 
