060c5cc4976b298a4b6ea9d440cfceccebacde0d
wolfenste <wolfenstecg@gmail.com>
2018-08-10 10:54:51 +0000

    Adding and passing the test: player construction with symbol 0.

--- tests/src/PlayerTest.php
+++ tests/src/PlayerTest.php
@@ -17,5 +17,13 @@ class PlayerTest extends BaseClassTest {
     public function the_player_should_not_be_constructed_with_symbol_none () {
         $playerX = new Player (new Mark (Mark::SYMBOL_NONE));
     }
+
+    /**
+     * @test
+     */
+    public function player_construction_with_symbol_0 () {
+        $player0 = new Player (new Mark (Mark::SYMBOL_0));
+        $this->assertTrue ((new Mark (Mark::SYMBOL_0))->equal ($player0->getPlayerName ()));
+    }
 }
 
