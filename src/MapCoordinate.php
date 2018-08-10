4eb026d98848f86bb5b76d28bcf18f886107e1db
wolfenste <wolfenstecg@gmail.com>
2018-08-10 13:33:25 +0000

    Adding coordinates to class MapCoordinate.

--- src/MapCoordinate.php
+++ src/MapCoordinate.php
@@ -7,5 +7,15 @@ class MapCoordinate {
     const ONE = 1;
     const TWO = 2;
     const THREE = 3;
+
+    /**
+     * @var integer between 1 and 3
+     */
+    private $coordinate_x;
+
+    /**
+     * @var integer between 1 and 3
+     */
+    private $coordinate_y;
 }
 
