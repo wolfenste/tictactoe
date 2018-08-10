5082db15d746df14503b346e7114b8bb76827086
wolfenste <wolfenstecg@gmail.com>
2018-08-10 13:40:39 +0000

    Passing the test: mapcoordinate is constructed with valid arguments.

--- src/MapCoordinate.php
+++ src/MapCoordinate.php
@@ -17,5 +17,18 @@ class MapCoordinate {
      * @var integer between 1 and 3
      */
     private $coordinate_y;
+
+    /**
+     * @param integer
+     * @param integer
+     */
+    public function __construct (int $x, int $y) {
+        if ($x < self::ONE || $x > self::THREE || $y < self::ONE || $y > self::THREE) {
+            throw \InvalidArgumentException ('Both arguments must be in interval [1, 3]');
+        }
+
+        $this->coordinate_x = $x;
+        $this->coordinate_y = $y;
+    }
 }
 
