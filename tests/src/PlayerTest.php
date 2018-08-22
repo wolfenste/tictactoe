39d4b36a4674aadce83fa7d5b8149cf87d965063
wolfenste <wolfenstecg@gmail.com>
2018-08-22 07:40:07 +0000

    Removing invalid tests from PlayerTest class.

--- tests/src/PlayerTest.php
+++ tests/src/PlayerTest.php
@@ -6,26 +6,6 @@ use TicTacToe\Mark;
 use TicTacToe\Map;
 
 class PlayerTest extends BaseClassTest {
-    public function test_player_construction_with_symbol_x () {
-	    $playerX = new Player (new Mark (Mark::SYMBOL_X));
-	$this->assertTrue ((new Mark (Mark::SYMBOL_X))->equal ($playerX->getPlayerName ()));
-    }
-
-    /**
-     * @test
-     * @expectedException InvalidArgumentException
-     */
-    public function the_player_should_not_be_constructed_with_symbol_none () {
-        $playerX = new Player (new Mark (Mark::SYMBOL_NONE));
-    }
-
-    /**
-     * @test
-     */
-    public function player_construction_with_symbol_0 () {
-        $player0 = new Player (new Mark (Mark::SYMBOL_0));
-        $this->assertTrue ((new Mark (Mark::SYMBOL_0))->equal ($player0->getPlayerName ()));
-    }
 
     /**
      * @test
