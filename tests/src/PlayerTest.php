edb6828aaaac76916751971766f44bf82c22ae53
wolfenste <wolfenstecg@gmail.com>
2018-08-22 08:03:19 +0000

    Adding test: testing player construction.

--- tests/src/PlayerTest.php
+++ tests/src/PlayerTest.php
@@ -1,11 +1,25 @@
 <?php
 
 use TicTacToeTests\BaseClassTest;
-use TicTacToe\Player;
+use TicTacToe\NextMoveProviderPlayer;
+use TicTacToe\NextMoveProviderAI;
 use TicTacToe\Mark;
 use TicTacToe\Map;
+use TicTacToe\Game;
 
 class PlayerTest extends BaseClassTest {
+    /**
+     * @test
+     */
+    public function testing_player_construction () {
+        $map = new Map ($this->createEmptyTableSpec ());
+        $option = null;
+        $strategyX = new NextMoveProviderPLayer ($map, $option);
+        $strategy0 = new NextMoveProviderPlayer ($map, $option);
+        $game = new Game ($strategyX, $strategy0, $map);
+        $this->assertTrue ((new Mark (Mark::SYMBOL_X))->equal ($game->getPlayerX ()));
+        $this->assertTrue ((new Mark (Mark::SYMBOL_0))->equal ($game->getPlayer0 ()));
+    }
 
     /**
      * @test
