8f101d00edc9d7b7e5ed0f9add9215de3d74732b
wolfenste <wolfenstecg@gmail.com>
2018-08-23 04:30:47 +0000

    Introducing the test: player strategy is constructed with an empty map and null option.

--- tests/src/NextMoveProviderPlayerTest.php
+++ tests/src/NextMoveProviderPlayerTest.php
@@ -6,4 +6,16 @@ use TicTacToe\MapCoordinate;
 use TicTacToe\Map;
 
 class NextMoveProviderPlayerTest extends BaseClassTest {
+    /**
+     * @test
+     */
+    public function player_strategy_is_constructed_with_an_empty_map_and_null_option () {
+        $map = new Map ($this->createEmptyTableSpec ());
+        $strategy = new NextMoveProviderPlayer ($map);
+        
+        $this->assertTrue (is_object ($strategy));
+        $this->assertTrue ($strategy instanceof NextMoveProvider);
+        $this->assertNull ($strategy->getPosition ());
+        $this->assertTrue ($map->isEmpty ());
+    }
 }
