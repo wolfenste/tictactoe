9d7791dc7edb67e353930512bd784e0a81a34619
wolfenste <wolfenstecg@gmail.com>
2018-08-23 00:26:20 +0000

    player x puts mark at map coordinate 2 2 test follows the new domain design.

--- tests/src/PlayerTest.php
+++ tests/src/PlayerTest.php
@@ -3,6 +3,7 @@
 use TicTacToeTests\BaseClassTest;
 use TicTacToe\NextMoveProviderPlayer;
 use TicTacToe\NextMoveProviderAI;
+use TicTacToe\MapCoordinate;
 use TicTacToe\Mark;
 use TicTacToe\Map;
 use TicTacToe\Game;
@@ -25,11 +26,14 @@ class PlayerTest extends BaseClassTest {
      * @test
      */
     public function player_x_puts_mark_at_mapcoordinate_2_2 () {
-        $playerX = new Player (new Mark (Mark::SYMBOL_0));
         $map = new Map ($this->createEmptyTableSpec ());
-        $this->assertTrue ($map->isMapAvailable (5));
-        $playerX->putMark (new MapCoordinate (MapCoordinate::TWO, MapCoordinate::TWO));
-        $this->assertFalse ($map->isMapAvailable (5));
+        $option = new MapCoordinate (MapCoordinate::TWO, MapCoordinate::TWO);
+        $strategyX = new NextMoveProviderPlayer ($map, $option);
+        $strategy0 = new NextMoveProviderAI ($map);
+        $game = new Game ($strategyX, $strategy0, $map);
+        $this->assertTrue ($map->isMapAvailable (4));
+        $game->getPlayerX ()->putMark ();
+        $this->assertTrue ((new Mark (Mark::SYMBOL_X))->equal ($map->getMarks () [4]));
     }
 }
 
