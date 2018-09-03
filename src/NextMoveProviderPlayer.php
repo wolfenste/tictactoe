ba1c4eb9f36f1ea45f1218b61276ba51cdd87ba4
wolfenste <wolfenstecg@gmail.com>
2018-09-03 09:03:44 +0000

    Introducing constructor for NextMoveProviderPlayer.

--- src/NextMoveProviderPlayer.php
+++ src/NextMoveProviderPlayer.php
@@ -10,9 +10,36 @@ class NextMoveProviderPlayer implements NextMoveProvider {
     private $position;
 
     /**
+     * @var instanceof ReadOnlyMap
+     */
+    private $map;
+
+    /**
+     * Class constructor
+     * @param instanceof ReadOnlyMap
+     * @param MapCoordinate object or null
+     */
+    public function __construct (ReadOnlyMap $map, $position = null) {
+        $this->map = $map;
+        $this->setPosition ($position);
+    }
+
+    /**
      * @return MapCoordinate object or null
      */
     public function getPosition () {
         return $this->position;
+    }
+
+    /**
+     * @param MapCoordinate object or null
+     * @return void
+     */
+    private function setPosition ($position) : void {
+        if ($position !== null && !($position instanceof MapCoordinate)) {
+            throw new \DomainException ('The parameter must be a MapCoordinate object or null.');
+        }
+        // To do: here must be checked the position availability in map
+        $this->position = $position;
     }
 }
