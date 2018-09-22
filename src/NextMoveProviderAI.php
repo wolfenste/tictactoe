e20c6c55cf610a213b044831ec2171537e3a3fbd
wolfenste <wolfenstecg@gmail.com>
2018-09-22 02:59:00 +0000

    Introducing some properties for NextMoveProviderAI.

--- src/NextMoveProviderAI.php
+++ src/NextMoveProviderAI.php
@@ -4,5 +4,24 @@ declare (strict_types = 1);
 namespace TicTacToe;
 
 class NextMoveProviderAI implements NextMoveProvider {
+    /**
+     * @var MapCoordinate object
+     */
+    private $position;
+
+    /** 
+     * @var Mark object
+     */
+    private $myMark;
+
+    /** 
+     * @var Mark object
+     */
+    private $oppMark;
+
+    /** 
+     * @var Map object, implements ReadOnlyMap interface
+     */
+    private $map;
 }
 
