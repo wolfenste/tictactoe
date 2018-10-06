b25c8468a487c8465ad6c042c30088c5ab606b4f
wolfenste <wolfenstecg@gmail.com>
2018-10-06 02:33:40 +0000

    NextMoveProviderAI will be an abstract class. Its test conforms to that.

--- tests/src/NextMoveProviderAITest.php
+++ tests/src/NextMoveProviderAITest.php
@@ -7,18 +7,22 @@ use TicTacToe\Map;
 use TicTacToe\NextMoveProvider;
 use TicTacToe\Mark;
 
+class ConcreteNextMoveProviderAI extends NextMoveProviderAI {
+    public function getNextMove () : MapCoordinate {
+        return new MapCoordinate (1, 1);
+    }
+}
+
 class NextMoveProviderAITest extends BaseClassTest {
     /**
      * @test
      */
     public function ai_strategy_is_constructed_with_symbol_x_and_an_empty_map () {
         $map = new Map ($this->createEmptyTableSpec ());
-        $strategy = new NextMoveProviderAI (new Mark (Mark::SYMBOL_X), $map);
+        $strategy = new ConcreteNextMoveProviderAI (new Mark (Mark::SYMBOL_X), $map);
         
-        $this->assertTrue (is_object ($strategy));
-        $this->assertTrue ($strategy instanceof NextMoveProvider);
         $this->assertTrue ($strategy->getMyMark ()->equal (new Mark (Mark::SYMBOL_X)));
-        $this->assertTrue ($map->isEmpty ());
+
     }
 
     /**
@@ -28,16 +32,6 @@ class NextMoveProviderAITest extends BaseClassTest {
      */
     public function ai_strategy_is_constructed_with_a_wrong_symbol () {
         $map = new Map ($this->createEmptyTableSpec ());
-        $strategy = new NextMoveProviderAI (new Mark (Mark::SYMBOL_NONE), $map);
-    }
-
-    /**
-     * @test
-     */
-    public function ai_next_move_is_a_valid_mapcoordinate_object () {
-        $map = new Map ($this->createEmptyTableSpec ());
-        $strategy = new NextMoveProviderAI (new Mark (Mark::SYMBOL_X), $map);
-
-        $this->assertTrue ($strategy->getNextMove () instanceof MapCoordinate);
+        $strategy = new ConcreteNextMoveProviderAI (new Mark (Mark::SYMBOL_NONE), $map);
     }
 }
