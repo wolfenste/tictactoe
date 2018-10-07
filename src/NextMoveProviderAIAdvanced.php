c5526cd70805815ca81af9cf92f2ebb7d25ab483
wolfenste <wolfenstecg@gmail.com>
2018-10-07 03:06:20 +0000

    Introducing NextMoveProviderAIAdvanced class.

---
+++ src/NextMoveProviderAIAdvanced.php
@@ -0,0 +1,12 @@
+<?php
+
+declare (strict_types = 1);
+namespace TicTacToe;
+
+class NextMoveProviderAIAdvanced extends NextMoveProviderAI {
+    /**
+     * @return MapCoordinate object or null
+     */
+    public function getNextMove () {
+    }
+}
